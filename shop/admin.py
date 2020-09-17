95d41cee8d8c48a5966369dd1d24b15b8613fecb
hamjihyeon <gkawlgus01@naver.com>
2020-09-17 10:44:34 +0900

    cart/cart Cart

--- shop/admin.py
+++ shop/admin.py
@@ -9,10 +9,9 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name','slug','category','price','available_display','available_order','created','updated']
+    list_display = ['name','slug','category','price','stock', 'available_display','available_order','created','updated']
     list_filter = ['available_display', 'created','updated','category']
     prepopulated_fields = {'slug':('name',)}
-    list_editable = ['price', 'stock', 'available_display', 'available_order'
-                                                            '']
+    list_editable = ['price', 'stock', 'available_display', 'available_order']
 
 admin.site.register(Product, ProductAdmin)
